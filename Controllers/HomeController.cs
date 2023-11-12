16802d6795434b96272d55cd46dcceb818219d3e
ArtemBgn <bgn.rtm@gmail.com>
2023-11-12 16:39:00 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ASP_SPD_222.Models;
+using ASP_SPD_222.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_SPD_222.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Transfer()
+        {
+            TransferViewModel model = new()
+            {
+                Date = DateOnly.FromDateTime(DateTime.Today),
+                Time = TimeOnly.FromDateTime(DateTime.Now),
+                ControllerName = this.GetType().Name,
+            };
+            return View(model);
+        }
+        public IActionResult Razor()
+        {
+            ViewData["formController"] = "Hello from Controller";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Home/TransferViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP_SPD_222.Models.Home
+{
+    /*
+     * Модель з даними, необхідна для відображення
+     * сторінки /Home/Transfer
+     */
+    public record TransferViewModel
+    {
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public String ControllerName { get; set; } = null!;
+    }
+}
